07371a9fe86a0deeb1520b54392eb1232eeeb1f1
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-04-15 14:50:26 +0200

    [INTERNAL, PARAM] For SpectraSTCreateAdapter, spectrast will now be executed
    Add spectra input files, library output file, and param file

--- src/utils/SpectraSTCreateAdapter.cpp
+++ src/utils/SpectraSTCreateAdapter.cpp
@@ -38,6 +38,7 @@
 #include <OpenMS/FORMAT/MzMLFile.h>
 #include <OpenMS/FORMAT/FileHandler.h>
 #include <OpenMS/SYSTEM/File.h>
+#include <OpenMS/DATASTRUCTURES/StringUtils.h>
 #include <QtCore/QProcess>
 #include <QDir>
 
@@ -67,39 +68,166 @@ using namespace std;
 /// @cond TOPPCLASSES
 
 class TOPPSpectraSTCreateAdapter :
-  public TOPPBase
+    public TOPPBase
 {
- public:
-  // Define parameter name
+  public:
+    // Define parameter name
+    static const String param_executable;
 
-  TOPPSpectraSTCreateAdapter() :
-    TOPPBase("SpectraSTCreateAdapter", "Interface to the CREATE Mode of the SpectraST executable", false)
-  {
-  }
+    static const String param_spectra_files;  // From which the library is to be generated
+    static const String param_spectra_files_formats;  // File formats being allowed for the input spectra
+    static const String param_params_file;  // Parameter file for library creation
+    static const String param_library_file;   // Library output
 
-protected:
+    TOPPSpectraSTCreateAdapter() :
+      TOPPBase("SpectraSTCreateAdapter", "Interface to the CREATE Mode of the SpectraST executable", false)
+    {
+    }
 
-  // this function will be used to register the tool parameters
-  // it gets automatically called on tool execution
-  void registerOptionsAndFlags_()
-  {
+  protected:
+
+    // this function will be used to register the tool parameters
+    // it gets automatically called on tool execution
+    void registerOptionsAndFlags_()
+    {
       StringList empty;
 
-  }
+      // Handle executable
+      registerInputFile_(TOPPSpectraSTCreateAdapter::param_executable, "<path>", "spectrast", "Path to the SpectraST executable to use; may be empty if the executable is globally available.", true, false, ListUtils::create<String>("skipexists"));
+
+      // Register input files for Spectra creation
+      registerInputFileList_(TOPPSpectraSTCreateAdapter::param_spectra_files, "<FileName1> [ <FileName2> ... <FileNameN> ]", empty, "File names(s) of spectra from which the library should be created.", true, false);
+      setValidFormats_(TOPPSpectraSTCreateAdapter::param_spectra_files, ListUtils::create<String>(TOPPSpectraSTCreateAdapter::param_spectra_files_formats), false);
+
+      // params file
+      registerInputFile_(TOPPSpectraSTCreateAdapter::param_params_file, "<params_file>", "", "Read create options from file. All options set in the file will be overridden by command-line options if specified.", false, false);
+      setValidFormats_(TOPPSpectraSTCreateAdapter::param_params_file, ListUtils::create<String>("params"), false);
+
+      // library (output) file
+      registerOutputFile_(TOPPSpectraSTCreateAdapter::param_library_file, "<library_file>", "", "Output library file", true, false);
+      setValidFormats_(TOPPSpectraSTCreateAdapter::param_library_file, ListUtils::create<String>("splib"), false);
+    }
 
 
-  // the main_ function is called after all parameters are read
-  ExitCodes main_(int, const char **)
-  {
+    // the main_ function is called after all parameters are read
+    ExitCodes main_(int, const char **)
+    {
+
+      // Assemble command line for SpectraST
+      QStringList arguments;
+
+      // Executable
+      String executable = getStringOption_(TOPPSpectraSTCreateAdapter::param_executable);
+      if (executable.empty())
+      {
+          executable = "spectrast";
+      }
+
+      // Make create mode explicit
+      arguments << "-c";
+
+      // Set the parameter file if present
+      String params_file = getStringOption_(TOPPSpectraSTCreateAdapter::param_params_file);
+      if ( ! params_file.empty())
+      {
+          arguments << params_file.toQString().prepend("-cF");
+      }
+
+      // Set the library (output) file
+      String library_file = getStringOption_(TOPPSpectraSTCreateAdapter::param_library_file);
+      if (library_file.empty())
+      {
+        LOG_ERROR << "ERROR: File for output library has not been specified. Terminating." << endl;
+        return ILLEGAL_PARAMETERS;
+      }
+      if ( ! library_file.hasSuffix("splib"))
+      {
+        LOG_ERROR << "ERROR: File for output library does not have the correct file ending (splib). Terminating." << endl;
+        return ILLEGAL_PARAMETERS;
+      }
+
+      // For spectrast, the file extension has to be removed
+      arguments << File::removeExtension(library_file).toQString().prepend("-cN");
 
 
-    // Exit the tool
-    return EXECUTION_OK;
-  }
+      // TODO Add more parameter
+
+
+      // Set the input files
+      StringList spectra_files = getStringList_(TOPPSpectraSTCreateAdapter::param_spectra_files);
+      if (spectra_files.size() < 1)
+      {
+        LOG_ERROR << "ERROR: At least one input file containing spectra must be provided" << endl;
+        return ILLEGAL_PARAMETERS;
+      }
+      String first_spectra_file = spectra_files[0];
+
+      // Figure out the input file format from the first provided file
+      StringList valid_spectra_formats;
+      String input_format;
+      StringUtils::split(TOPPSpectraSTCreateAdapter::param_spectra_files_formats, ",", valid_spectra_formats);
+      for (StringList::const_iterator valid_spectra_formats_it = valid_spectra_formats.begin();
+           valid_spectra_formats_it != valid_spectra_formats.end(); ++valid_spectra_formats_it)
+      {
+        String current_format = *valid_spectra_formats_it;
+        if (first_spectra_file.hasSuffix(current_format))
+        {
+          input_format = current_format;
+          break;
+        }
+      }
+
+      // Exit if spectra format has not been recognized
+      if (input_format.empty())
+      {
+        LOG_ERROR << "ERROR: Unrecognized input format from spectra file: " << first_spectra_file << endl;
+        return ILLEGAL_PARAMETERS;
+      }
+
+      // Add all input files. Make sure that the file ending is the same as defined from the first provided file
+      for (StringList::const_iterator spectra_files_it = spectra_files.begin();
+           spectra_files_it != spectra_files.end(); ++spectra_files_it)
+      {
+        String spectra_file = *spectra_files_it;
+        if ( ! spectra_file.hasSuffix(input_format))
+        {
+          LOG_ERROR << "ERROR: Input spectra file does not agree in format: "
+                         << spectra_file << " is not " << input_format << endl;
+          return ILLEGAL_PARAMETERS;
+        }
+        arguments << spectra_file.toQString();
+      }
+
+      // Write command-line call to DEBUG_LOG
+      LOG_DEBUG << "COMMAND: " << executable;
+      for (QStringList::const_iterator it = arguments.begin(); it != arguments.end(); ++it)
+      {
+          LOG_DEBUG << " " << it->toStdString();
+      }
+      LOG_DEBUG << endl;
+
+
+      // Run spectrast
+      QProcess spectrast_process;
+      spectrast_process.start(executable.toQString(), arguments);
+      if (! spectrast_process.waitForFinished(-1))
+      {
+          LOG_ERROR << "Fatal error running SpectraST\nDoes the spectrast executable exist?" << endl;
+          return EXTERNAL_PROGRAM_ERROR;
+      }
+
+
+      // Exit the tool
+      return EXECUTION_OK;
+    }
 };
 // End of Tool definition
 
-
+const String TOPPSpectraSTCreateAdapter::param_executable = "executable";
+const String TOPPSpectraSTCreateAdapter::param_spectra_files = "spectra_files";
+const String TOPPSpectraSTCreateAdapter::param_spectra_files_formats = "msp,hlf,pepXML,pep.xml,xml,ms2,splib";
+const String TOPPSpectraSTCreateAdapter::param_params_file = "params_file";
+const String TOPPSpectraSTCreateAdapter::param_library_file = "library_file";
 
 
 
